623222d0d5c080325c8d42e32315d8669a97367e
Amirah Chamble <amirah.chamble@yahoo.com>
2018-06-09 15:52:09 +0000

    Changes to css and attempt to add Particle.js

---
+++ js/particlejs.js
@@ -0,0 +1,15 @@
+/* Everything regarding the Particle Background below*/
+var particles;
+
+particles = Particles.init({
+  selector : ".img-particle-js",
+  color: ["#F8A944", "#B38436", "#EFF7E8", "#445660"],
+  connectParticles : true,
+  sizeVariations: 4 ,
+  maxParticles : 150,
+  speed: .2
+});
+
+/** *^^^^^ this requires the following javascript library to work: 
+https://cdnjs.cloudflare.com/ajax/libs/particlesjs/2.2.2/particles.js
+*/
