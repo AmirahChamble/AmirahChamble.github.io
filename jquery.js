10e08ce4c6c69bce4457c1fad1dd2c31aa55d590
Amirah Chamble <amirahchamble@Amirahs-MBP.fios-router.home>
2018-06-24 12:53:25 -0400

    particle js is working

---
+++ jquery.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+
+
+  $("nav div a").click(function(){
+    $("body,html").animate({
+      scrollTop:$("#" +  $(this).data('value')).offset().top
+  },1000)
+  })
+});
--- particlejs.js
+++ particlejs.js
@@ -1,28 +1,61 @@
-jQuery(document).ready(function($) {
-
-
-  $("nav div a").click(function(){
-    $("body,html").animate({
-      scrollTop:$("#" +  $(this).data('value')).offset().top
-  },1000)
-  })
-});
-
-
-/*************Canvas**************/
 /* Everything regarding the Particle Background below*/
-var particles;
 
-particles = Particles.init({
-  selector : ".particlejs",
-  color: ["#f3f6b4", "#57b9c6", "#3c34d2", "#fdf3bf", "#fbddc5" , "#d523c6"],
-  connectParticles : false,
-  sizeVariations: 4 ,
-  maxParticles : 150,
-  speed: .2
-});
+window.onload = function() {
+   Particles.init({
+    selector : ".particles-js",
+    color: ["#f3f6b4", "#57b9c6", "#3c34d2", "#fdf3bf", "#fbddc5" , "#d523c6"],
+    connectParticles : false,
+    sizeVariations: 10 ,
+    maxParticles : 150,
+    speed: .5,
+    hover: "bubble",
+
+
+// options for breakpoints
+ responsive: [
+   {
+     breakpoint:
+768
+,
+     options: {
+       maxParticles:
+200
+,
+       color:
+'#48F2E3'
+,
+       connectParticles:
+false
+     }
+   }, {
+     breakpoint:
+425
+,
+     options: {
+       maxParticles:
+100
+,
+       connectParticles:
+true
+     }
+   }, {
+     breakpoint:
+320
+,
+     options: {
+       maxParticles:
+0
+
+// disables particles.js
+     }
+   }
+ ]
+
+  });
+
+};
+
+
 /** *^^^^^ this requires the following javascript library to work:
 https://cdnjs.cloudflare.com/ajax/libs/particlesjs/2.2.2/particles.js
-color: ["#f3f6b4", "#57b9c6", "#3c34d2", "#fdf3bf", "#fbddc5" , "#d523c6"],
-
 */
