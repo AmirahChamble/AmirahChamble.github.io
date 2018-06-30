1e010d501246f096fbd87ae2874ec2ac9ae741e4
Amirah Chamble <amirahchamble@Amirahs-MBP.fios-router.home>
2018-06-30 10:42:05 -0400

    changes to site

--- jquery.js
+++ jquery.js
@@ -4,6 +4,12 @@ jQuery(document).ready(function($) {
   $("nav div a").click(function(){
     $("body,html").animate({
       scrollTop:$("#" +  $(this).data('value')).offset().top
-  },1000)
-  })
+  },1400);
+  });
+
+  /*function toggleClass(elem) {
+    $(elem).toggleClass('active');
+  };*/
+
+
 });
