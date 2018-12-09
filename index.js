bf236c5faca65578c62ec3eff9c3ea06f37d6e89
Amirah Chamble <amirahchamble@Amirahs-MBP.fios-router.home>
2018-12-09 17:14:46 -0500

    more changes to tabs to make them awesome

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function hideTabs(){
-
   var tabContent = document.getElementsByClassName("Contentwrapper");
   //hide all blocks wth this class name
   for(var i = 0; i < tabContent.length; i++ ){
@@ -7,23 +6,21 @@ function hideTabs(){
   }
 }
 
-
-function openTab(event, tab){
-  //some variables
-  var i , tabLinks ;
-
-  //hide all elements with the class="tabContnet"
-  hideTabs();
-
-  //remove active class from all class="tablinks"
-  tabLinks = document.getElementsByClassName("tabWrapper");
-  for( i =0; i < tabLinks.length; i++){
-    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+function removeActiveClass(){
+  var tabLinks = document.getElementsByClassName("tabWrapper");
+  for( var i =0; i < tabLinks.length; i++){
+    tabLinks[i].className = tabLinks[i].className.replace("active", "");
   }
-  //show current tab content section and add the active status to its tab
-  document.getElementById(tab).style.display = "flex";
-  event = currentTarget.className += " active"
+}
 
-  
+
+function openTab(tab, tabContent){
+  //hide all elements with the class="tabContent"
+  hideTabs();
+  //remove active class from all class="tabLinks"
+  removeActiveClass();
+  //show current tab content section and add the active status to its tab
+  document.getElementById(tabContent).style.display = "flex";
+  document.getElementById(tab).classList.add("active");
 
 }
